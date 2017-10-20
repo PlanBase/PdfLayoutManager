db14a06db27c1cc91c77a7266ad2af5746587e3d
Glen K. Peterson <glen@organicdesign.org>
2017-10-20 19:19:07 -0400

    Added another test for renderablesToTextLines().

--- src/test/java/com/planbase/pdf/layoutmanager/TextLineKtTest.kt
+++ src/test/java/com/planbase/pdf/layoutmanager/TextLineKtTest.kt
@@ -30,7 +30,7 @@ class TextLineKtTest {
 //    @Ignore
 
     fun verifyLine(line:TextLine, lineHeight:Float, maxWidth:Float, text:String) {
-        println("line: " + line)
+//        println("line: " + line)
         assertEquals(lineHeight, line.height(), floatCloseEnough)
         assertTrue(line.width < maxWidth)
         assertEquals(text,
@@ -49,7 +49,7 @@ class TextLineKtTest {
         val maxWidth = 60f
 
         val textLines: List<TextLine> = renderablesToTextLines(listOf(txt1, txt2, txt3), maxWidth)
-        println(textLines)
+//        println(textLines)
 
         assertEquals(3, textLines.size)
 
@@ -60,21 +60,22 @@ class TextLineKtTest {
         verifyLine(textLines[2], tStyle1.lineHeight(), maxWidth, "great stuff.")
     }
 
-//    @Test fun testRenderablesToLines2() {
-//        val tStyle1 = TextStyle(PDType1Font.HELVETICA, 9f, Utils.CMYK_BLACK)
-//        val txt1 = Text.of(tStyle1, "Hello ")
-//        val tStyle2 = TextStyle(PDType1Font.HELVETICA_BOLD, 13f, Utils.CMYK_BLACK)
-//        val txt2 = Text.of(tStyle2, "there ")
-//        val txt3 = Text.of(tStyle1, "world! This is great stuff.")
-//
-//        val textLines: List<TextLine> = renderablesToTextLines(listOf(txt1, txt2, txt3), 50f)
-////        println(textLines)
-//
-//        assertEquals(4, textLines.size)
-//
-//        val line1 = textLines[0]
-//        println("line1: " + line1)
-//        assertEquals(tStyle2.lineHeight(), line1.height())
-//    }
+    @Test fun testRenderablesToLines2() {
+        val tStyle1 = TextStyle(PDType1Font.HELVETICA, 9f, Utils.CMYK_BLACK)
+        val txt1 = Text.of(tStyle1, "Hello ")
+        val tStyle2 = TextStyle(PDType1Font.HELVETICA_BOLD, 13f, Utils.CMYK_BLACK)
+        val txt2 = Text.of(tStyle2, "there ")
+        val txt3 = Text.of(tStyle1, "world! This is great stuff.")
+        val maxWidth = 90f
+
+        val textLines: List<TextLine> = renderablesToTextLines(listOf(txt1, txt2, txt3), maxWidth)
+//        println(textLines)
+
+        assertEquals(2, textLines.size)
+
+        verifyLine(textLines[0], tStyle2.lineHeight(), maxWidth, "Hello there world!")
+
+        verifyLine(textLines[1], tStyle1.lineHeight(), maxWidth, "This is great stuff.")
+    }
 
 }
